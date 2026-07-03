9e158c0a9e95a452817112bbfb3d8df1fd35750a
Mateus Filipe Souza Martins <mateus.11881423@aluno.mg.gov.br>
2026-07-03 10:33:56 -0300

    colocando uma forma de editar as casas

--- jogo_da_velha/src/JVelha.java
+++ jogo_da_velha/src/JVelha.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; // Importamos isso para conseguir ler o que o usuário digita
+import java.util.Scanner;
 
 public class JVelha {
     public static void main(String[] args) {
@@ -26,9 +26,161 @@ public class JVelha {
         int jogada = teclado.nextInt();
 
         if (jogada == 1)
+            if (tabuleiro[0][0] == '1') {
+                tabuleiro[0][0] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 2)
+            if (tabuleiro[0][1] == '2') {
+                tabuleiro[0][1] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 3)
+            if (tabuleiro[0][2] == '3') {
+                tabuleiro[0][2] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 4)
+            if (tabuleiro[1][0] == '4') {
+                tabuleiro[1][0] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 5)
+            if (tabuleiro[1][1] == '5') {
+                tabuleiro[1][1] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 6)
+            if (tabuleiro[1][2] == '6') {
+                tabuleiro[1][2] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 7)
+            if (tabuleiro[2][0] == '7') {
+                tabuleiro[2][0] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 8)
+            if (tabuleiro[2][1] == '8') {
+                tabuleiro[2][1] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
+
+        if (jogada == 9)
+            if (tabuleiro[2][2] == '9') {
+                tabuleiro[2][2] = 'X';
+                System.out.println("Você escolheu a posição: " + jogada);
+
+                System.out.println("\n");
+                System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
+                System.out.println("-----------");
+                System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
+                System.out.println("-------------------------\n");
+
+            } else {
+                System.out.println("Essa posição já está ocupada! Escolha outra.");
+            }
 
 
-        System.out.println("Você escolheu a posição: " + jogada);
+
+        //System.out.println("Você escolheu a posição: " + jogada);
 
 
         teclado.close();
