40a19eb9b5e6550dc3a4e9e8f780b8c6d0a55252
Mateus Filipe Souza Martins <mateus.11881423@aluno.mg.gov.br>
2026-07-08 10:18:16 -0300

    arrumando os erros brutais e deixando o código desnecessariamente grande pela minha incopetência.

--- jogo_da_velha/src/JVelha2.java
+++ jogo_da_velha/src/JVelha2.java
@@ -1,21 +1,36 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class JVelha {
+public class JVelha2 {
     public static void main(String[] args) {
-
-
-
         char[][] tabuleiro = {
                 {'1', '2', '3'},
                 {'4', '5', '6'},
                 {'7', '8', '9'}
         };
-
         Scanner teclado = new Scanner(System.in);
-        char jogadorAtual = 'X';
-
+        char jogadorAtual = '✘';
+        char jogadorIA = '✷';
+        boolean CS1 = false;
+        boolean CS2 = false;
+        boolean CS3 = false;
+        boolean CS4 = false;
+        boolean CS5 = false;
+        boolean CS6 = false;
+        boolean CS7 = false;
+        boolean CS8 = false;
+        boolean CS9 = false;
+        boolean xCS1 = false;
+        boolean xCS2 = false;
+        boolean xCS3 = false;
+        boolean xCS4 = false;
+        boolean xCS5 = false;
+        boolean xCS6 = false;
+        boolean xCS7 = false;
+        boolean xCS8 = false;
+        boolean xCS9 = false;
         while (true) {
+            //---------------------------------------------------------------------------------------------------------
             System.out.println("\n");
             System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
             System.out.println("-----------");
@@ -23,151 +38,264 @@ public class JVelha {
             System.out.println("-----------");
             System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
             System.out.println("-------------------------\n");
-
             System.out.print("Turno do [" + jogadorAtual + "] - Escolha uma posição livre (1 a 9): ");
             int jogada = teclado.nextInt();
-            boolean jogadaValida = false;
-
             if (jogada == 1) {
                 if (tabuleiro[0][0] == '1') {
                     tabuleiro[0][0] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS1 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 2) {
+            else if (jogada == 2) {
                 if (tabuleiro[0][1] == '2') {
                     tabuleiro[0][1] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS2 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 3) {
+            else if (jogada == 3) {
                 if (tabuleiro[0][2] == '3') {
                     tabuleiro[0][2] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS3 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 4) {
+            else if (jogada == 4) {
                 if (tabuleiro[1][0] == '4') {
                     tabuleiro[1][0] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS4 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 5) {
+            else if (jogada == 5) {
                 if (tabuleiro[1][1] == '5') {
                     tabuleiro[1][1] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS5 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 6) {
+            else if (jogada == 6) {
                 if (tabuleiro[1][2] == '6') {
                     tabuleiro[1][2] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS6 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 7) {
+            else if (jogada == 7) {
                 if (tabuleiro[2][0] == '7') {
                     tabuleiro[2][0] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS7 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 8) {
+            else if (jogada == 8) {
                 if (tabuleiro[2][1] == '8') {
                     tabuleiro[2][1] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS8 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-            if (jogada == 9) {
+            else if (jogada == 9) {
                 if (tabuleiro[2][2] == '9') {
                     tabuleiro[2][2] = jogadorAtual;
                     System.out.println("Você escolheu a posição: " + jogada);
-                    jogadaValida = true;
+                    CS9 = true;
                 } else {
                     System.out.println("Ocupado!");
                 }
             }
-
-                    java.util.Random gerador = new java.util.Random();
-                    int jogadaComputador = gerador.nextInt(9) + 1;
-                    System.out.println("O computador escolheu a posição: " + jogadaComputador);
-                    if (jogadaComputador == 1) {
-
-                        if (tabuleiro[0][0] == '1') {
-                            tabuleiro[0][0] = O;
-                            System.out.println("A IA escolheu a posição: " + jogada);
-                            jogadaValida = true;
-                        } else {
-                            System.out.println("Ocupado!");
-                        }
-
-                    }
-
-            //if (jogadaValida) {
-            //    if (verificarGanhador(tabuleiro)) {
-            //        System.out.println(" FIM DE JOGO! O JOGADOR [" + jogadorAtual + "] VENCEU!");
-//
-             //        System.out.print("\n");
-            //        System.out.print(" " + tabuleiro[0][0] + " | " + tabuleiro[0][1] + " | " + tabuleiro[0][2] + "\n");
-            //        System.out.println("-----------");
-            //        System.out.print(" " + tabuleiro[1][0] + " | " + tabuleiro[1][1] + " | " + tabuleiro[1][2] + "\n");
-             //       System.out.println("-----------");
-            //        System.out.print(" " + tabuleiro[2][0] + " | " + tabuleiro[2][1] + " | " + tabuleiro[2][2] + "\n");
-//
-              //       break;
-             //   }
-
-
-
-            }
-
-           // public static boolean verificarGanhador(char[][] tabuleiro) {
-           //     for (int i = 0; i < 3; i++) {
-           //         if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
-           //             return true;
-           //         }
-           //     }
-           //     for (int j = 0; j < 3; j++) {
-           //         if (tabuleiro[0][j] == tabuleiro[1][j] && tabuleiro[1][j] == tabuleiro[2][j]) {
-           //             return true;
-           //         }
-           //     }
-           //     if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
-           //         return true;
-           //     }
-           //     if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
-           //         return true;
-           //     }
-           //     return false;
-           // }
-
+//---------------------------------------------------------------------------------------------------
+            java.util.Random gerador = new java.util.Random();
+            int jogadaComputador = gerador.nextInt(9) + 1;
+            System.out.println("O computador escolheu a posição: " + jogadaComputador);
+            if (jogadaComputador == 1) {
+                if (tabuleiro[0][0] == '1') {
+                    tabuleiro[0][0] = jogadorIA;
+                    xCS1 = true;
                 }
             }
+            else if (jogadaComputador == 2) {
+                if (tabuleiro[0][1] == '2') {
+                    tabuleiro[0][1] = jogadorIA;
+                    xCS2 = true;
+                }
+            }
+            else if (jogadaComputador == 3) {
+                if (tabuleiro[0][2] == '3') {
+                    tabuleiro[0][2] = jogadorIA;
+                    xCS3 = true;
+                }
+            }
+            else if (jogadaComputador == 4) {
+                if (tabuleiro[1][0] == '4') {
+                    tabuleiro[1][0] = jogadorIA;
+                    xCS4 = true;
+                }
+            }
+            else if (jogadaComputador == 5) {
+                if (tabuleiro[1][1] == '5') {
+                    tabuleiro[1][1] = jogadorIA;
+                    xCS5 = true;
+                }
+            }
+            else if (jogadaComputador == 6) {
+                if (tabuleiro[1][2] == '6') {
+                    tabuleiro[1][2] = jogadorIA;
+                    xCS6 = true;
+                }
+            }
+            else if (jogadaComputador == 7) {
+                if (tabuleiro[2][0] == '7') {
+                    tabuleiro[2][0] = jogadorIA;
+                    xCS7 = true;
+                }
+            }
+            else if (jogadaComputador == 8) {
+                if (tabuleiro[2][1] == '8') {
+                    tabuleiro[2][1] = jogadorIA;
+                    xCS8 = true;
+                }
+            }
+            else if (jogadaComputador == 9) {
+                if (tabuleiro[2][2] == '9') {
+                    tabuleiro[2][2] = jogadorIA;
+                    xCS9 = true;
+                }
 
-
-
-        teclado.close();
+            }
+//-------------------------------------------------------------------------------------
+            if (tabuleiro[0][0] == jogadorAtual && tabuleiro[0][1] == jogadorAtual && tabuleiro[0][2] == jogadorAtual) {
+            //xxx
+            //...
+            //...
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[1][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[1][2] == jogadorAtual) {
+                //...
+                //xxx
+                //...
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[2][0] == jogadorAtual && tabuleiro[2][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
+                //...
+                //...
+                //xxx
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[1][0] == jogadorAtual && tabuleiro[2][0] == jogadorAtual && tabuleiro[3][0] == jogadorAtual) {
+                //x..
+                //x..
+                //x..
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[0][1] == jogadorAtual && tabuleiro[2][1] == jogadorAtual && tabuleiro[3][1] == jogadorAtual) {
+                //.x.
+                //.x.
+                //.x.
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][2] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
+                //..x
+                //..x
+                //..x
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
+                //x..
+                //.x.
+                //..x
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+            if (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual) {
+                //..x
+                //.x.
+                //x..
+                System.out.println("o Jogador venceu!");
+                break;
+            }
+//-------------------------------------------------------------------------------------
+            if (tabuleiro[0][0] == jogadorIA && tabuleiro[0][1] == jogadorIA && tabuleiro[0][2] == jogadorIA) {
+                //ooo
+                //...
+                //...
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[1][0] == jogadorIA && tabuleiro[1][1] == jogadorIA && tabuleiro[1][2] == jogadorIA) {
+                //...
+                //ooo
+                //...
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[2][0] == jogadorIA && tabuleiro[2][1] == jogadorIA && tabuleiro[2][2] == jogadorIA) {
+                //...
+                //...
+                //ooo
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[1][0] == jogadorIA && tabuleiro[2][0] == jogadorIA && tabuleiro[3][0] == jogadorIA) {
+                //o..
+                //o..
+                //o..
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[0][1] == jogadorIA && tabuleiro[2][1] == jogadorIA && tabuleiro[3][1] == jogadorIA) {
+                //.o.
+                //.o.
+                //.o.
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[0][2] == jogadorIA && tabuleiro[1][2] == jogadorIA && tabuleiro[2][2] == jogadorIA) {
+                //..o
+                //..o
+                //..o
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[0][0] == jogadorIA && tabuleiro[1][1] == jogadorIA && tabuleiro[2][2] == jogadorIA) {
+                //o..
+                //.o.
+                //..o
+                System.out.println("a IA venceu!");
+                break;
+            }
+            if (tabuleiro[0][2] == jogadorIA && tabuleiro[1][1] == jogadorIA && tabuleiro[2][0] == jogadorIA) {
+                //..o
+                //.o.
+                //o..
+                System.out.println("a IA venceu!");
+                break;
+            }
+        }
+    }
+}
 
 
