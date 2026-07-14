33d1bee4741e4f62b35532bf6c6313c9f3154e51
MatsHubTch <mateus.11881423@aluno.mg.gov.br>
2026-07-13 21:06:15 -0300

    Update JVelha2.java

--- jogo_da_velha/src/JVelha2.java
+++ jogo_da_velha/src/JVelha2.java
@@ -12,10 +12,15 @@ public class JVelha2 {
         //1 □  □  □
         //2 □  □  □
         Scanner teclado = new Scanner(System.in);
-        char j_A = '✘';
+        char j_Atual = '✘';
         char j_IA = '✷';
+        int roberto;
+        int pqsim;
+        int tentativas = 0;
         while (true) {
             //-----------------------------MINHA JOGADA----------------------------------------------------------------
+            roberto = 1;
+            pqsim = 1;
             System.out.println("\n");
             System.out.print(" " + tabu[0][0] + " | " + tabu[0][1] + " | " + tabu[0][2] + "\n");
             System.out.println("-----------");
@@ -23,56 +28,163 @@ public class JVelha2 {
             System.out.println("-----------");
             System.out.print(" " + tabu[2][0] + " | " + tabu[2][1] + " | " + tabu[2][2] + "\n");
             System.out.println("-------------------------\n");
-            System.out.print("Turno do [" + j_A + "] - Escolha uma posição livre (1 a 9): ");
+            System.out.print("Turno do [" + j_Atual + "] - Escolha uma posição livre (1 a 9): ");
             int jogada = teclado.nextInt();
             int linha = (jogada - 1) / 3;
             int coluna = (jogada - 1) % 3;
             if (tabu[linha][coluna] != '✘' &&
                     tabu[linha][coluna] != '✷') {
 
-                tabu[linha][coluna] = j_A;
-            }
-            else {
+                tabu[linha][coluna] = j_Atual;
+            } else {
                 System.out.println("Casa ocupada!");
             }
-//-------------------------------------JOGADA DA IA------------------------------------------------
-            java.util.Random gerador = new java.util.Random();
-            int j_Comp = gerador.nextInt(9) + 1;
-            System.out.println("O computador escolheu a posição: " + j_Comp);
-            linha = (j_Comp - 1) / 3;
-            coluna = (j_Comp - 1) % 3;
-            if (tabu[linha][coluna] != '✘' &&
-                    tabu[linha][coluna] != '✷') {
 
-                tabu[linha][coluna] = j_IA;
-            }
-            else {
-                System.out.println("Casa ocupada, IA!");
-            }
-//------------------------------------INTELIGÊNCIA DA IA-------------------------------------
-            //NULL
-// ------------------------------------MINHAS VITÓRIAS-------------------------------------
-            if (tabu[0][0] == j_A && tabu[0][1] == j_A && tabu[0][2] == j_A
-                    || tabu[1][0] == j_A && tabu[1][1] == j_A && tabu[1][2] == j_A
-                    || tabu[2][0] == j_A && tabu[2][1] == j_A && tabu[2][2] == j_A
-                    || tabu[0][0] == j_A && tabu[1][0] == j_A && tabu[2][0] == j_A
-                    || tabu[0][1] == j_A && tabu[1][1] == j_A && tabu[2][1] == j_A
-                    || tabu[0][2] == j_A && tabu[1][2] == j_A && tabu[2][2] == j_A
-                    || tabu[0][0] == j_A && tabu[1][1] == j_A && tabu[2][2] == j_A
-                    || tabu[0][2] == j_A && tabu[1][1] == j_A && tabu[2][0] == j_A) {
+            // ------------------------------------MINHAS VITÓRIAS-------------------------------------
+            if (tabu[0][0] == j_Atual && tabu[0][1] == j_Atual && tabu[0][2] == j_Atual
+                    || tabu[1][0] == j_Atual && tabu[1][1] == j_Atual && tabu[1][2] == j_Atual
+                    || tabu[2][0] == j_Atual && tabu[2][1] == j_Atual && tabu[2][2] == j_Atual
+                    || tabu[0][0] == j_Atual && tabu[1][0] == j_Atual && tabu[2][0] == j_Atual
+                    || tabu[0][1] == j_Atual && tabu[1][1] == j_Atual && tabu[2][1] == j_Atual
+                    || tabu[0][2] == j_Atual && tabu[1][2] == j_Atual && tabu[2][2] == j_Atual
+                    || tabu[0][0] == j_Atual && tabu[1][1] == j_Atual && tabu[2][2] == j_Atual
+                    || tabu[0][2] == j_Atual && tabu[1][1] == j_Atual && tabu[2][0] == j_Atual) {
                 System.out.println("o Jogador venceu!");
                 break;
             }
+
 //-----------------------------------VITÓRIAS DA IA-------------------------------------
             if (tabu[0][0] == j_IA && tabu[0][1] == j_IA && tabu[0][2] == j_IA
                     || tabu[1][0] == j_IA && tabu[1][1] == j_IA && tabu[1][2] == j_IA
                     || tabu[2][0] == j_IA && tabu[2][1] == j_IA && tabu[2][2] == j_IA
-                    || tabu[1][0] == j_IA && tabu[2][0] == j_IA && tabu[3][0] == j_IA
-                    || tabu[0][1] == j_IA && tabu[2][1] == j_IA && tabu[3][1] == j_IA
+                    || tabu[0][0] == j_IA && tabu[1][0] == j_IA && tabu[2][0] == j_IA
+                    || tabu[0][1] == j_IA && tabu[1][1] == j_IA && tabu[2][1] == j_IA
                     || tabu[0][2] == j_IA && tabu[1][2] == j_IA && tabu[2][2] == j_IA
                     || tabu[0][0] == j_IA && tabu[1][1] == j_IA && tabu[2][2] == j_IA
                     || tabu[0][2] == j_IA && tabu[1][1] == j_IA && tabu[2][0] == j_IA) {
-                System.out.println("a IA venceu!");
+                System.out.println("O Roberto venceu!");
                 break;
             }
+//-------------------------------------JOGADA DO ROBERTO------------------------------------------------
+            //-------------------------------------ATAQUE-----------------------------------------------
+
+
+
+            //-------------------------------------DEFENDE-----------------------------------------------
+while (roberto == 1) {
+                if (tabu[0][0] == j_Atual && tabu[0][1] == j_Atual
+                        || tabu[2][2] == j_Atual && tabu[1][2] == j_Atual
+                        || tabu[2][0] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[0][2] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][1] == j_Atual && tabu[0][2] == j_Atual
+                        || tabu[1][0] == j_Atual && tabu[2][0] == j_Atual
+                        || tabu[2][2] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[0][0] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][0] == j_Atual && tabu[1][0] == j_Atual
+                        || tabu[2][1] == j_Atual && tabu[2][2] == j_Atual
+                        || tabu[0][2] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[2][0] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][2] == j_Atual && tabu[1][2] == j_Atual
+                        || tabu[2][0] == j_Atual && tabu[2][1] == j_Atual
+                        || tabu[0][0] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[2][2] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][0] == j_Atual && tabu[2][2] == j_Atual
+                        || tabu[2][0] == j_Atual && tabu[0][2] == j_Atual
+                        || tabu[1][0] == j_Atual && tabu[1][2] == j_Atual
+                        || tabu[0][1] == j_Atual && tabu[2][1] == j_Atual) {
+                    tabu[1][1] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][0] == j_Atual && tabu[0][2] == j_Atual
+                        || tabu[2][1] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[0][1] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+                if (tabu[0][2] == j_Atual && tabu[2][2] == j_Atual
+                        || tabu[1][0] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[1][2] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[2][0] == j_Atual && tabu[2][2] == j_Atual
+                        || tabu[0][1] == j_Atual && tabu[1][1] == j_Atual) {
+                    tabu[2][1] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+
+
+                if (tabu[0][0] == j_Atual && tabu[2][0] == j_Atual
+                        || tabu[1][1] == j_Atual && tabu[1][2] == j_Atual) {
+                    tabu[1][0] = j_IA;
+                    roberto = 0;
+                    break;
+                }
+    roberto = 0;
+
+        java.util.Random gerador = new java.util.Random();
+    while (pqsim == 1) {
+        int j_Comp = gerador.nextInt(9) + 1;
+        linha = (j_Comp - 1) / 3;
+        coluna = (j_Comp - 1) % 3;
+
+        if (tabu[linha][coluna] != '✘' && tabu[linha][coluna] != '✷') {
+            tabu[linha][coluna] = j_IA;
+            pqsim = 0;
+        }
+
+        tentativas++;
+        if (tentativas > 50) { //mas pq logo 50? pq sim.
+            pqsim = 0;
+        }
+    }
+
+
+            }
+//------------------------------------INTELIGÊNCIA DA IA-------------------------------------
+                //NULL
+
+        }
+    }
+}
             // 13/07 (11:19) "tá difícil de pensar em como a ia vai parar de ser burra..!"
+//            java.util.Random gerador = new java.util.Random();
+//int j_Comp = gerador.nextInt(9) + 1;
+//            System.out.println("O computador escolheu a posição: " + j_Comp);
+//linha = (j_Comp - 1) / 3;
+//coluna = (j_Comp - 1) % 3;
+//        if (tabu[linha][coluna] != '✘' &&
+//tabu[linha][coluna] != '✷') {
+//
+//tabu[linha][coluna] = j_IA;
+//            }
+//                    else {
+//                    System.out.println("Casa ocupada, IA!");
+//            }
